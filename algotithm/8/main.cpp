d4ef02b4dd395d68b6f235c786f396931903b5a0
wxx <614610440@qq.com>
2019-09-22 10:15:44 +0800

    update 8

--- algotithm/8/main.cpp
+++ algotithm/8/main.cpp
@@ -2,64 +2,114 @@
 
 using namespace std;
 
+//class Solution {
+//public:
+//    int myAtoi(string str) {
+//        int max = 0x7fffffff;
+//        int min = 0x80000000;
+//        long result = 0;
+//        int first_cha = 0;
+//
+//        for (int i = 0; i < str.size(); ++i) {
+//            if (str[i] == ' ') {
+//                if (first_cha != 0) {
+//                    return first_cha * result;
+//                }
+//                continue;
+//            }
+//
+//            if (first_cha == 0) {
+//                if (str[i] == '-') {
+//                    first_cha = -1;
+//                    continue;
+//                } else if (str[i] == '+') {
+//                    first_cha = 1;
+//                    continue;
+//                } else if (str[i] >= '0' && str[i] <= '9'){
+//                    first_cha = 1;
+//                } else {
+//                    return 0;
+//                }
+//            }
+//
+//            if (str[i] >= '0' && str[i] <= '9') {
+//                result = result * 10 + (str[i] - '0');
+//            } else {
+//                if (result > max) {
+//                    if (first_cha == -1) {
+//                        return min;
+//                    }
+//                    return max;
+//                } else {
+//                    return first_cha * result;
+//                }
+//            }
+//
+//            if (result > max) {
+//                if (first_cha == -1) {
+//                    return min;
+//                }
+//                return max;
+//            }
+//        }
+//
+//        result = first_cha * result;
+//
+//        if (result < min) {
+//            return min;
+//        } else if (result > max) {
+//            return  max;
+//        }
+//        return result;
+//    }
+//};
+
 class Solution {
 public:
     int myAtoi(string str) {
         int max = 0x7fffffff;
         int min = 0x80000000;
         long result = 0;
-        int first_cha = 0;
+        int first_cha = 1;
 
-        for (int i = 0; i < str.size(); ++i) {
-            if (str[i] == ' ') {
-                if (first_cha != 0) {
-                    return first_cha * result;
-                }
-                continue;
-            }
+        int index = 0;
 
-            if (first_cha == 0) {
-                if (str[i] == '-') {
-                    first_cha = -1;
-                    continue;
-                } else if (str[i] == '+') {
-                    first_cha = 1;
-                    continue;
-                } else if (str[i] >= '0' && str[i] <= '9'){
-                    first_cha = 1;
-                } else {
-                    return 0;
-                }
-            }
+        while (str[index] == ' ') {
+            ++index;
+        }
 
-            if (str[i] >= '0' && str[i] <= '9') {
-                result = result * 10 + (str[i] - '0');
-            } else {
+        if (index >= str.size()) {
+            return 0;
+        }
+
+        if (str[index] == '-') {
+            first_cha = -1;
+            ++index;
+        } else if (str[index] == '+') {
+            ++index;
+        } else if (str[index] < '0' || str[index] > '9') {
+            return 0;
+        }
+
+        for (; index < str.size(); ++index) {
+            if (str[index] >= '0' && str[index] <= '9') {
                 if (result > max) {
-                    if (first_cha == -1) {
-                        return min;
-                    }
-                    return max;
-                } else {
-                    return first_cha * result;
+                    break;
                 }
-            }
-
-            if (result > max) {
-                if (first_cha == -1) {
-                    return min;
-                }
-                return max;
+                result = result * 10 + (str[index] - '0');
+            } else {
+                break;
             }
         }
 
-        result = first_cha * result;
+        result *= first_cha;
 
         if (result < min) {
             return min;
         } else if (result > max) {
-            return  max;
+            return max;
         }
+
         return result;
     }
 };
