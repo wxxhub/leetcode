68ef0d40d4c4e971f717741ee67742e7ee356787
wxx <614610440@qq.com>
2019-11-01 18:38:59 +0800

    add 21

--- README.md
+++ README.md
@@ -32,6 +32,7 @@
 [18.四数之和~](algotithm/18/README.md)  
 [19.删除链表的倒数第N个节点](algotithm/19/README.md)  
 [20.有效的括号](algotithm/20/README.md)  
+[21.合并两个有序链表](algotithm/21/README.md)
 
 #### 排序算法
 [1.快排序](sort/fast_sort/README.md)  
---
+++ algotithm/21/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void pushNode(ListNode *&node, int i) {
+    ListNode *new_node = new ListNode(i);
+    new_node->next = node;
+    node = new_node;
+}
+
+void printNode(ListNode *l) {
+    ListNode *index = l;
+
+    while (index != NULL) {
+        cout << index->val << " ";
+        index = index->next;
+    }
+}
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) {
+            return l2;
+        }
+
+        if (l2 == NULL) {
+            return l1;
+        }
+        ListNode *index1 = l1;
+        ListNode *index2 = l2;
+        ListNode *result;
+        ListNode *result_index;
+
+        if (index1->val > index2->val) {
+            result = index2;
+            index2 = index2->next;
+        } else {
+            result = index1;
+            index1 = index1->next;
+        }
+
+        result_index = result;
+
+        while (index1 != NULL && index2 != NULL) {
+            if (index1->val > index2->val) {
+                result_index->next = index2;
+                index2 = index2->next;
+            } else {
+                result_index->next = index1;
+                index1 = index1->next;
+            }
+            result_index = result_index->next;
+        }
+
+        if (index1 != NULL) {
+            result_index->next = index1;
+        } else {
+            result_index->next = index2;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    ListNode *list1 = new ListNode(4);
+    ListNode *list2 = new ListNode(4);
+
+    pushNode(list1, 2);
+    pushNode(list1, 1);
+
+    pushNode(list2, 3);
+    pushNode(list2, 1);
+
+    printNode(solution.mergeTwoLists(list1, list2));
+    return 0;
+}
